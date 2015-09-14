2b533bdf002de64bd0497cbae83dd356557b68f9
Matt Caywood <caywood@gmail.com>
2015-09-14 00:54:56 -0400

    Add upload page, call to validator (not yet working)

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This is a hosted version of the [General Transit Feed Spec (GTFS)](https://developers.google.com/transit/gtfs/) feed validator (feedvalidator.py) from the [transitfeed](https://github.com/google/transitfeed/wiki) project.
 
 It's a Flask app, [running on Heroku](https://devcenter.heroku.com/articles/getting-started-with-python-o).
+
+http://flask.pocoo.org/docs/0.10/installation/
+
+http://flask.pocoo.org/docs/0.10/quickstart/#a-minimal-application
+
+http://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
+
+http://flask.pocoo.org/docs/0.10/patterns/fileuploads/#uploading-files
--- hello.py
+++ hello.py
@@ -1,10 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, url_for, request
+from feedvalidator import RunValidationOutputToFile
+
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+	return app.send_static_file('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['gtfsfeedzip']
+        output_filename = 'results.html'
+        exit_code = RunValidationOutputToFilename(f,'',output_filename)
+        # Exit code is 2 if an extension is provided but can't be loaded, 1 if
+    	# problems are found and 0 if the Schedule is problem free.
+    	# plain text string is '' if no other problems are found.
+    	return send_from_directory(output_filename)
 
 if __name__ == '__main__':
-    app.run()
-    
+  app.run(debug=True)
