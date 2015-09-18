950c8099e85123ffc4e482fbb252d96b4e947bb6
Matt Caywood <caywood@gmail.com>
2015-09-18 02:08:18 -0400

    Add URL validation

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, url_for, send_file
 from feedvalidator import RunValidationOutputToFilename, ParseCommandLineArguments
+import urllib
+import StringIO
 import transitfeed
 from transitfeed import util
+
+output_filename = 'validation-results.html'
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -12,28 +16,36 @@ def index():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-    	print request
-        f = request.files['gtfsfeedzip']
-        output_filename = 'validation-results.html'
+      f = request.files['gtfsfeedzip']
+      parse_file(f)
+      return send_file(output_filename)
 
-        parser = util.OptionParserLongError(
-          usage='', version='%prog '+transitfeed.__version__)
+@app.route('/validate', methods=['GET', 'POST'])
+def upload_url():
+    if request.method == 'POST':
+      url = request.form['gtfsfeedurl']
+      f = StringIO.StringIO(urllib.urlopen(url).read())
+      parse_file(f)
+      return send_file(output_filename)
 
-        parser.set_defaults(manual_entry=False, output=output_filename,
-                      memory_db=False, check_duplicate_trips=False,
-                      limit_per_type=5, latest_version='',
-                      service_gap_interval=13)
-        (options, args) = parser.parse_args()
-        options.error_types_ignore_list = None
-        options.extension = None
+def parse_file(feed):
+  # feed: GTFS file, either path of the file as a string or a file object
 
-        exit_code = RunValidationOutputToFilename(f,options,output_filename)
-        # Exit code is 2 if an extension is provided but can't be loaded, 1 if
-    	# problems are found and 0 if the Schedule is problem free.
-    	# plain text string is '' if no other problems are found.
-    	# return send_from_directory(output_filename)
+  parser = util.OptionParserLongError(usage='', version='%prog '+transitfeed.__version__)
+  parser.set_defaults(manual_entry=False, output=output_filename,
+                memory_db=False, check_duplicate_trips=False,
+                limit_per_type=5, latest_version='',
+                service_gap_interval=13)
+  (options, args) = parser.parse_args()
+  options.error_types_ignore_list = None
+  options.extension = None
 
-    	return send_file(output_filename)
+  exit_code = RunValidationOutputToFilename(feed,options,output_filename)
+  # Exit code is 2 if an extension is provided but can't be loaded, 1 if
+  # problems are found and 0 if the Schedule is problem free.
+  # plain text string is '' if no other problems are found.
+  # return send_from_directory(output_filename)
+  return exit_code
 
 if __name__ == '__main__':
   app.run(debug=True)
